b26026be0a6bf693a8a9903268ec5b677fd506e4
Gopala chachre <chachregopala@gmail.com>
2026-09-16 00:30:31 +0530

    typeof

--- JS/basics/02dataType.js
+++ JS/basics/02dataType.js
@@ -45,3 +45,29 @@ let shopFunction = function() {
     console.log("This is a function");
 };
 shopFunction();
+
+// or function can also be declared using arrow function
+let shopArrowFunction = () => {
+    console.log("This is an arrow function");
+};
+shopArrowFunction();
+
+// and function can also be declared using function declaration
+function shopFunctionDeclaration() {
+    console.log("This is a function declaration");
+}
+shopFunctionDeclaration();
+
+// now typeof operator can be used to check the data type of a variable
+console.log(typeof age); // number
+console.log(typeof username); // string
+console.log(typeof isOpen); // boolean
+console.log(typeof shopDiscount); // object (this is a known issue in JavaScript, null is considered an object)
+console.log(typeof shopAddress); // undefined
+console.log(typeof bigNumber); // bigint
+console.log(typeof uniqueId); // symbol
+console.log(typeof shopItems); // object (arrays are considered objects in JavaScript)
+console.log(typeof shopDetails); // object
+console.log(typeof shopFunction); // function
+console.log(typeof shopArrowFunction); // function
+console.log(typeof shopFunctionDeclaration); // function
